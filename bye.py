a2ce0f5e1efec981d4aeab006e6ac230e643128a
Ly Nguyen <lnguyengwc@gmail.com>
2024-03-30 01:50:22 -0400

    fall detection

--- bye.py
+++ bye.py
@@ -1,5 +1,46 @@
+import cv2
+import math
 from ultralytics import YOLO
 
-model = YOLO('yolov8m-pose.pt')
+model = YOLO('yolov8n-pose.pt')
+vid = cv2.VideoCapture(0) 
 
-results = model(source=0, show=True, conf=0.3, save=True)
+while vid.isOpened():
+    success, frame = vid.read()
+
+    results = model(frame)
+
+    for info in results:
+        parameters = info.boxes
+        for box in parameters:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            confidence = box.conf[0]
+            conf = math.ceil(confidence * 100)
+
+            height = y2 - y1
+            width = x2 - x1
+            threshold = height - width
+
+            if conf > 80:
+                cv2.rectangle(frame, (x1,y1), (x1+x2, y1+y2), (0,255,0), 2)
+            if threshold < 0:
+                cv2.rectangle(frame, (x1,y1), (x1+x2, y1+y2), (0,0,255), 2)
+
+            else: pass
+    
+    # results = model.predict(source=0, show=True, conf=0.5, imgsz=320,save=True)
+
+    # annotated_frame = results[0].plot()
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # else:
+    #     break
+
+vid.release()
+cv2.destoryAllWindows()
+

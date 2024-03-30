59302928395ebaf38c86b94013b3bad660869a40
Ly Nguyen <lnguyengwc@gmail.com>
2024-03-30 03:55:19 -0400

    stops video when remaining in fall

--- bye.py
+++ bye.py
@@ -1,5 +1,6 @@
 import cv2
 import math
+import time
 from ultralytics import YOLO
 
 model = YOLO('yolov8n-pose.pt')
@@ -8,6 +9,11 @@ vid = cv2.VideoCapture(0)
 classnames = []
 with open('SteelHacks2024/classes.txt', 'r') as f:
     classnames = f.read().splitlines()
+
+starting_time = time.time()
+begin = starting_time
+falling = False
+hold = 0
 
 while vid.isOpened():
     success, frame = vid.read()
@@ -31,17 +37,35 @@ while vid.isOpened():
 
             font = cv2.FONT_HERSHEY_SIMPLEX
 
+            if falling:
+                curr_elapsed = time.time() - starting_time
+                hold = hold + curr_elapsed
+                print(hold)
+                if hold > 5:
+                    print("FALL")
+                    vid.release()
+                    cv2.destroyAllWindows()
+                    # print("Elapsed: {}".format(curr_elapsed))
+            else:
+                hold = 0
+
             if conf > 80:
+                falling = False
                 cv2.rectangle(frame, (x1,y1), (x1+x2, y1+y2), (0,255,0), 2)
+                # elapsed_time = time.time() - starting_time
+                # print("Elapsed: {}".format(elapsed_time))
             if threshold < 0 and class_detect == 'person' and y1 > 325:
+                starting_time = time.time()
+                falling = True
                 cv2.rectangle(frame, (x1,y1), (x1+x2, y1+y2), (0,0,255), 2)
                 cv2.putText(frame,  
-                'Fall Detected',  
+                'Fall Detected: calling in 3s',  
                 (50, 50),  
                 font, 1,  
                 (0, 255, 255),  
                 2,  
                 cv2.LINE_4) 
+                # elapsed_time = time.time() - starting_time
 
             else: pass
     

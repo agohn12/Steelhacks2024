cb962eae3ec5fb4da040eeb46d191c1cb69837fc
Mle50 <125671779+Mle50@users.noreply.github.com>
2024-03-29 22:31:56 -0400

    People?

--- Test1.py
+++ Test1.py
@@ -1,3 +1,23 @@
-print("Hello World!")
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-print("TEST")
+# import the opencv library
+import cv2
+
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+
+while(True):
+
+    # Capture the video frame by frame
+    ret, frame = vid.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    # the 'q' button is set as the quitting button you may use any desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
